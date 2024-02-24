04479c87c017ce7b64dec7326a7821fee412b590
wallace wambulwa <125740903+wallacetrixie@users.noreply.github.com>
2024-02-24 03:14:57 +0300

    Add files via upload

---
+++ src/footer.jsx
@@ -0,0 +1,8 @@
+function Footer() { 
+    return (
+        <footer>
+            <p>&copy; {new Date().getFullYear()} wallace wambulwa happy to learn react</p>
+        </footer>
+    );
+};
+export default Footer;
---
+++ src/languages.jsx
@@ -0,0 +1,20 @@
+function Programming() { 
+
+    const v1 = "c";
+    const v2 = "python";
+    const v3 = "c++";
+    const v4 = "javascript";
+    const v5 = "React js";
+    return (
+        <ul>
+        <h2>list of poppular programming languages</h2>
+            <li>{ v1.toUpperCase()}</li>
+            <li>{ v2.toUpperCase()}</li>
+            <li>{ v3.toUpperCase()}</li>
+            <li>{ v4.toUpperCase()}</li>
+            <li>{ v5.toUpperCase()}</li>
+        </ul>
+    );
+};
+
+export default Programming;
