75bf3cf2fb5a91558c5263da8a8e1d9316457110
wallace wambulwa <125740903+wallacetrixie@users.noreply.github.com>
2024-02-24 03:27:50 +0300

    Delete src directory

--- src/footer.jsx
+++
@@ -1,8 +0,0 @@
-function Footer() { 
-    return (
-        <footer>
-            <p>&copy; {new Date().getFullYear()} wallace wambulwa happy to learn react</p>
-        </footer>
-    );
-};
-export default Footer;
